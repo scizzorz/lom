17a2b155b23b8c1f420b08d43ad6c829f65828df
John Weachock <jweachock@gmail.com>
2021-03-31 17:19:58 -0400

    sprite: Add ox/oy and Aiming class

--- src/sprite.lua
+++ src/sprite.lua
@@ -11,6 +11,8 @@ function Sprite:init(id)
   self.size = framesets[atlas[id].frameset].size
   self.sx = 1
   self.sy = 1
+  self.ox = 0
+  self.oy = 0
   self.angle = 0
   self.frame = 0
   self.visible = true
@@ -24,6 +26,26 @@ function Sprite:draw()
     love.graphics.setColor(255, 255, 255)
     love.graphics.draw(self.gfx, self.quads[self.frame],
                       S(self.x), S(self.y), self.angle,
-                      SCALE * self.sx, SCALE * self.sy)
+                      SCALE * self.sx, SCALE * self.sy,
+                      self.ox, self.oy)
   end
+end
+
+Aiming = Object:extend()
+
+function Aiming:init(tex, w, h, ox, oy)
+  self.gfx = load_gfx(tex)
+  self.w = w
+  self.h = h
+  self.ox = ox
+  self.oy = oy
+  self.angle = 0
+  self.x = 0
+  self.y = 0
+  self.quad = love.graphics.newQuad(0, 0, w, h, w, h)
+end
+
+function Aiming:draw()
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.draw(self.gfx, self.quad, S(self.x), S(self.y), self.angle, SCALE, SCALE, self.ox, self.oy)
 end
