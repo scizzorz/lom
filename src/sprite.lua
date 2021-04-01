5081486123b7f04e7e092b4b83fa0380aa200b4a
John Weachock <jweachock@gmail.com>
2021-04-01 13:37:13 -0400

    sprite: Add support for flipped cards

--- src/sprite.lua
+++ src/sprite.lua
@@ -43,18 +43,26 @@ function Card:init(tex)
   self.ox = 15
   self.oy = 21
   self.angle = 0
+  self.flip = 0
+  self.tflip = 0
   self.quad = love.graphics.newQuad(0, 0, 30, 42, 30, 42)
   self.tex = load_gfx(tex)
+  self.back = load_gfx("card_back")
 end
 
 function Card:update()
   self.x = self.x + (self.tx - self.x) / 4
   self.y = self.y + (self.ty - self.y) / 4
+  self.flip = self.flip + (self.tflip - self.flip) / 7
 end
 
 function Card:draw()
   love.graphics.setColor(255, 255, 255)
-  love.graphics.draw(self.tex, self.quad, S(self.x), S(self.y), self.angle, SCALE, SCALE, self.ox, self.oy)
+  if self.flip > 0.5 then
+    love.graphics.draw(self.back, self.quad, S(self.x), S(self.y), self.angle, SCALE * 2 * (self.flip - 0.5), SCALE, self.ox, self.oy)
+  else
+    love.graphics.draw(self.tex, self.quad, S(self.x), S(self.y), self.angle, SCALE * 2 * (0.5 - self.flip), SCALE, self.ox, self.oy)
+  end
 end
 
 function Card:castable()
