fa89ca7f14cadb7241b66adeced5678743845ba1
John Weachock <jweachock@gmail.com>
2021-04-16 22:47:12 -0400

    data: Change Sprint's start/endlag and impulse

--- src/data.lua
+++ src/data.lua
@@ -15,12 +15,10 @@ card_db = {
     name = "Sprint",
     art = "card_sprint",
     cost = 2,
-    startlag = 0.5,
-    endlag = 2,
     cast = function(caster, x, y)
       local dir = math.angle(caster.x, caster.y, x, y)
-      local dx = math.cos(dir) * 200
-      local dy = math.sin(dir) * 200
+      local dx = math.cos(dir) * 300
+      local dy = math.sin(dir) * 300
       caster.body:applyLinearImpulse(dx, dy)
     end
   },
