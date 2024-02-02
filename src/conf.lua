b066e6b20d36006d6c111c3ab0f54e53694610ae
John Weachock <jweachock@gmail.com>
2024-02-02 10:24:22 -0500

    Rename to Legend of Mango

--- src/conf.lua
+++ src/conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
   t.version = "11.5"
-  t.window.title = "Heroes of Might & Mango"
+  t.window.title = "Legend of Mango"
   t.window.width = 1200
   t.window.height = 675
   t.window.fullscreen = false
